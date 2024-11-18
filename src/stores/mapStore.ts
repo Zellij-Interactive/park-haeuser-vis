631e9f4bcff036e2de4adf4e274e6b02f3f674c8
Karim Ouhammi <ouhammik@gmail.com>
2024-11-19 00:28:24 +0100

    update map store

--- src/stores/mapStore.ts
+++ src/stores/mapStore.ts
@@ -1,32 +1,96 @@
 import { useLocalStorage } from '@vueuse/core';
 import { type Marker } from '@/types/marker';
-import type { ParkingGarageInfo } from '@/types/parkingGarage';
-import type { ParkingGarageData } from '@/types/parkingGarageData';
+import type { ParkingGarageInfo, ParkingGarageInfoRaw } from '@/types/parkingGarage';
+import type { ParkingGarageData, ParkingGarageDataRaw } from '@/types/parkingGarageData';
 import * as d3 from 'd3';
 import { timeParse } from 'd3';
 import { _throw } from '@/core/_throw';
+import type { ParkingGarageRMSERaw } from '@/types/parkingGarageRMSE';
 
 export const mainzCoordinates = useLocalStorage<Marker>('MAINZ_LOCATION', {
     latitude: 49.97947979124793,
     longitude: 8.265089599253555,
 });
 
+export enum ParkingGarageNames {
+    Bleiche = 'Bleiche',
+    Cinestar = 'Cinestar',
+    CityPort = 'CityPort',
+    Deutschhausplatz = 'Deutschhausplatz',
+    Loehrstrasse = 'Loehrstrasse',
+    Rheinufergarage = 'Rheinufergarage',
+    RoemischesTheater = 'RoemischesTheater',
+    Schillerplatz = 'Schillerplatz',
+    Taubertsberg = 'Taubertsberg',
+    Augustusplatz = 'Augustusplatz',
+}
+
 export const parkingGaragesInfos = useLocalStorage<ParkingGarageInfo[]>(
     'PARKING_GARAGES_INFOS',
     []
 );
-export const parkingGaragesData = useLocalStorage<ParkingGarageData[]>('PARKING_GARAGES_DATA', []);
+
+export const bleiche = useLocalStorage<ParkingGarageData[]>('BLEICHE', []);
+export const cinestar = useLocalStorage<ParkingGarageData[]>('CINESTAR', []);
+export const cityPort = useLocalStorage<ParkingGarageData[]>('CITY_PORT', []);
+export const deutschhausplatz = useLocalStorage<ParkingGarageData[]>('DEUTSCHHAUSPLATZ', []);
+export const loehrstrasse = useLocalStorage<ParkingGarageData[]>('LOEHRSTRASSE', []);
+export const rheinufergarage = useLocalStorage<ParkingGarageData[]>('RHEINUFERGARAGE', []);
+export const roemischesTheater = useLocalStorage<ParkingGarageData[]>('ROEMISCHES_THEATER', []);
+export const schillerplatz = useLocalStorage<ParkingGarageData[]>('SCHILLERPLATZ', []);
+export const taubertsberg = useLocalStorage<ParkingGarageData[]>('TAUBERTSBERG', []);
+export const augustusplatz = useLocalStorage<ParkingGarageData[]>('AUGUSTUSPLATZ', []);
 
 loadParkingGaragesInfos().then((data) => {
-    parkingGaragesInfos.value = data;
+    //console.log(data);
+    //parkingGaragesInfos.value = data;
 });
 
-loadParkingGaragesData().then((data) => {
-    console.log(data);
-    parkingGaragesData.value = data;
+loadParkingGaragesRMSE().then((data) => {
+    //console.log(data);
 });
 
-async function loadParkingGaragesInfos(): Promise<ParkingGarageInfo[]> {
+for (const garageName of Object.values(ParkingGarageNames)) {
+    loadParkingGaragesData(garageName).then((data) => {
+        switch (garageName) {
+            case ParkingGarageNames.Bleiche:
+                bleiche.value = data;
+                break;
+            case ParkingGarageNames.Cinestar:
+                cinestar.value = data;
+                break;
+            case ParkingGarageNames.CityPort:
+                cityPort.value = data;
+                break;
+            case ParkingGarageNames.Deutschhausplatz:
+                deutschhausplatz.value = data;
+                break;
+            case ParkingGarageNames.Loehrstrasse:
+                loehrstrasse.value = data;
+                break;
+            case ParkingGarageNames.Rheinufergarage:
+                rheinufergarage.value = data;
+                break;
+            case ParkingGarageNames.RoemischesTheater:
+                roemischesTheater.value = data;
+                break;
+            case ParkingGarageNames.Schillerplatz:
+                schillerplatz.value = data;
+                break;
+            case ParkingGarageNames.Taubertsberg:
+                taubertsberg.value = data;
+                break;
+            case ParkingGarageNames.Augustusplatz:
+                augustusplatz.value = data;
+                break;
+            default:
+                _throw('Illegal state.');
+                break;
+        }
+    });
+}
+
+async function loadParkingGaragesInfos(): Promise<ParkingGarageInfoRaw[]> {
     try {
         const dsv = d3.dsvFormat(';');
 
@@ -41,7 +105,7 @@ async function loadParkingGaragesInfos(): Promise<ParkingGarageInfo[]> {
 
         return rawData.map((d) => ({
             name: d.Name,
-            maximalOccupancy: +d['Maximale Belegung in Parkplaetze'],
+            maximalOccupancy: parseInt(d['Maximale Belegung in Parkplaetze']),
             location: {
                 latitude: parseFloat(d.Latitude.replace(',', '.')),
                 longitude: parseFloat(d.Longitude.replace(',', '.')),
@@ -53,11 +117,36 @@ async function loadParkingGaragesInfos(): Promise<ParkingGarageInfo[]> {
     }
 }
 
-async function loadParkingGaragesData(): Promise<ParkingGarageData[]> {
+async function loadParkingGaragesRMSE(): Promise<ParkingGarageRMSERaw[]> {
     try {
         const dsv = d3.dsvFormat(';');
 
-        const response = await fetch('./src/data/Parkhausdaten.csv');
+        const response = await fetch('./src/data/RMSEListe.csv');
+
+        if (!response) {
+            _throw('Failed to fetch CSV.');
+        }
+
+        const csvText = await response.text();
+        const rawData = dsv.parse(csvText);
+
+        return rawData.map((d) => ({
+            name: d['Parkhaus'],
+            time: getTimeOfDay(d['Zeit']),
+            rmse: parseFloat(d['RMSE'].replace(',', '.')),
+            meanValue: parseFloat(d['Wahrer Mittelwert'].replace(',', '.')),
+        }));
+    } catch (error) {
+        console.error('Error processing CSV data:', error);
+        return [];
+    }
+}
+
+async function loadParkingGaragesData(garageName: string): Promise<ParkingGarageDataRaw[]> {
+    try {
+        const dsv = d3.dsvFormat(';');
+
+        const response = await fetch(`./src/data/${garageName}.csv`);
 
         if (!response) {
             _throw('Failed to fetch CSV.');
@@ -70,24 +159,32 @@ async function loadParkingGaragesData(): Promise<ParkingGarageData[]> {
 
         return rawData.map((d) => ({
             date: parseDate(d.Datum) ?? undefined,
-            infos: undefined,
-            prediction: +d.Prediction,
-            shapEducation: +d['SHAP_Education'],
-            shapServicesSpecialtyRetail: +d['SHAP_Services and specialty retail'],
-            shapFinanceInsurance: +d['SHAP_Finance and insurance'],
-            shapLeisureTime: +d['SHAP_Leisure time'],
-            shapFoodServices: +d['SHAP_Food services'],
-            shapHealth: +d['SHAP_Health'],
-            shapGrocery: +d['SHAP_Grocery'],
-            shapReligion: +d['SHAP_Religion'],
-            shapShopping: +d['SHAP_Shopping'],
-            shapOthers: +d['SHAP_Others'],
-            shapPublicSector: +d['SHAP_Public sector'],
-            shapTime: +d['SHAP_Time'],
-            shapMonth: +d['SHAP_Month'],
+            name: garageName,
+            prediction: parseFloat(d.Prediction.replace(',', '.')),
+            shapEducation: parseFloat(d['SHAP_Education'].replace(',', '.')),
+            shapServicesSpecialtyRetail: parseFloat(
+                d['SHAP_Services and specialty retail'].replace(',', '.')
+            ),
+            shapFinanceInsurance: parseFloat(d['SHAP_Finance and insurance'].replace(',', '.')),
+            shapLeisureTime: parseFloat(d['SHAP_Leisure time'].replace(',', '.')),
+            shapFoodServices: parseFloat(d['SHAP_Food services'].replace(',', '.')),
+            shapHealth: parseFloat(d['SHAP_Health'].replace(',', '.')),
+            shapGrocery: parseFloat(d['SHAP_Grocery'].replace(',', '.')),
+            shapReligion: parseFloat(d['SHAP_Religion'].replace(',', '.')),
+            shapShopping: parseFloat(d['SHAP_Shopping'].replace(',', '.')),
+            shapOthers: parseFloat(d['SHAP_Others'].replace(',', '.')),
+            shapPublicSector: parseFloat(d['SHAP_Public sector'].replace(',', '.')),
+            shapTime: parseFloat(d['SHAP_Time'].replace(',', '.')),
+            shapMonth: parseFloat(d['SHAP_Month'].replace(',', '.')),
+            shapSum: parseFloat(d['SHAP_Sum'].replace(',', '.')),
         }));
     } catch (error) {
         console.error('Error processing CSV data:', error);
         return [];
     }
+}
+
+function getTimeOfDay(input: string): number {
+    const match = input.match(/^(\d+),/);
+    return match ? parseInt(match[1], 10) : -1;
 }
