267b4495a8d27eed4dd7063cf12dc9cba268678a
Karim Ouhammi <ouhammik@gmail.com>
2024-11-19 00:44:42 +0100

    adjust mapStore accordantly

--- src/stores/mapStore.ts
+++ src/stores/mapStore.ts
@@ -6,6 +6,7 @@ import * as d3 from 'd3';
 import { timeParse } from 'd3';
 import { _throw } from '@/core/_throw';
 import type { ParkingGarageRMSERaw } from '@/types/parkingGarageRMSE';
+import { openDB } from 'idb';
 
 export const mainzCoordinates = useLocalStorage<Marker>('MAINZ_LOCATION', {
     latitude: 49.97947979124793,
@@ -42,51 +43,17 @@ export const taubertsberg = useLocalStorage<ParkingGarageData[]>('TAUBERTSBERG',
 export const augustusplatz = useLocalStorage<ParkingGarageData[]>('AUGUSTUSPLATZ', []);
 
 loadParkingGaragesInfos().then((data) => {
-    //console.log(data);
-    //parkingGaragesInfos.value = data;
+    storeInIndexedDB('PARKING_GARAGES_INFOS', data);
 });
 
 loadParkingGaragesRMSE().then((data) => {
-    //console.log(data);
+    //storeInIndexedDB('PARKING_GARAGES_RMSE', data);
 });
 
 for (const garageName of Object.values(ParkingGarageNames)) {
     loadParkingGaragesData(garageName).then((data) => {
-        switch (garageName) {
-            case ParkingGarageNames.Bleiche:
-                bleiche.value = data;
-                break;
-            case ParkingGarageNames.Cinestar:
-                cinestar.value = data;
-                break;
-            case ParkingGarageNames.CityPort:
-                cityPort.value = data;
-                break;
-            case ParkingGarageNames.Deutschhausplatz:
-                deutschhausplatz.value = data;
-                break;
-            case ParkingGarageNames.Loehrstrasse:
-                loehrstrasse.value = data;
-                break;
-            case ParkingGarageNames.Rheinufergarage:
-                rheinufergarage.value = data;
-                break;
-            case ParkingGarageNames.RoemischesTheater:
-                roemischesTheater.value = data;
-                break;
-            case ParkingGarageNames.Schillerplatz:
-                schillerplatz.value = data;
-                break;
-            case ParkingGarageNames.Taubertsberg:
-                taubertsberg.value = data;
-                break;
-            case ParkingGarageNames.Augustusplatz:
-                augustusplatz.value = data;
-                break;
-            default:
-                _throw('Illegal state.');
-                break;
-        }
+        //TODO add more garage infos before saving
+        storeInIndexedDB(garageName, data);
     });
 }
 
@@ -182,6 +149,22 @@ async function loadParkingGaragesData(garageName: string): Promise<ParkingGarage
         console.error('Error processing CSV data:', error);
         return [];
     }
+}
+
+// Store data in IndexedDB
+async function storeInIndexedDB(key: string, value: any) {
+    const db = await openDB('parking-garage-db', 1, {
+        upgrade(db) {
+            db.createObjectStore('keyval');
+        },
+    });
+    await db.put('keyval', value, key);
+}
+
+// Retrieve data from IndexedDB
+async function retrieveFromIndexedDB(key: string) {
+    const db = await openDB('parking-garage-db', 1);
+    return await db.get('keyval', key);
 }
 
 function getTimeOfDay(input: string): number {
