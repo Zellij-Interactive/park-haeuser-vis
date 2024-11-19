36ce50952cc8fddb12409d96c3f87af69adab4ac
Karim Ouhammi <ouhammik@gmail.com>
2024-11-19 16:43:49 +0100

    update store

--- src/stores/parkingGarageStore.ts
+++ src/stores/parkingGarageStore.ts
@@ -1,3 +1,17 @@
 import { defineStore } from 'pinia';
+import type { ParkingGarage } from '@/types/parkingGarage';
 
-export const useParkingGarageStore = defineStore('parking-garage', {});
+export const useParkingGarageStore = defineStore('parking-garage', {
+    state: () => ({
+        parkingGarages: new Array<ParkingGarage>(),
+    }),
+
+    actions: {
+        async loadAllParkingGarages(): Promise<ParkingGarage[]> {
+            return [];
+        },
+        async getAllParkingGarages(): Promise<ParkingGarage[]> {
+            return [];
+        },
+    },
+});
