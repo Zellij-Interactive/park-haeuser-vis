4d05b211992ae3253cb8a83eb107fa95c76ef6c1
Karim Ouhammi <ouhammik@gmail.com>
2024-11-21 20:49:30 +0100

    remove amcharts component

--- src/date-time-filter/views/DateTimeFilterView.vue
+++ src/date-time-filter/views/DateTimeFilterView.vue
@@ -1,10 +1,7 @@
 <template>
     <h1>DateTimeFilter</h1>
-    <ChartTest />
 </template>
 
-<script setup lang="ts">
-import ChartTest from '../components/ChartTest.vue';
-</script>
+<script setup lang="ts"></script>
 
 <style></style>
