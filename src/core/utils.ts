abdfadf46289c90f4ee12bb4192b9085588212e3
Karim Ouhammi <ouhammik@gmail.com>
2024-11-22 19:06:53 +0100

    add colorPalette record

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,4 +1,5 @@
-import { _throw } from "./_throw";
+import { _throw } from './_throw';
+import { colorPalette } from './colorPalette';
 
 export function getCircleRadius(maximalOccupancy: number): number {
     return maximalOccupancy / 10;
@@ -6,12 +7,12 @@ export function getCircleRadius(maximalOccupancy: number): number {
 
 export function getColorSaturation(prediction: number) {
     if (prediction < 0 || prediction > 100) {
-        _throw('Prediction value is out of bounds. Expected range: [0, 1]. Received: ' + prediction)
+        _throw(
+            'Prediction value is out of bounds. Expected range: [0, 1]. Received: ' + prediction
+        );
     }
 
-    const colorPalette = ['#eff3ff', '#bdd7e7', '#6baed6', '#3182bd', '#08519c']
+    const index = Math.min(Math.floor(prediction / 20), 4);
 
-    const index = Math.min(Math.floor(prediction / 20), colorPalette.length - 1);
-
-    return colorPalette[index]
+    return colorPalette[index];
 }
