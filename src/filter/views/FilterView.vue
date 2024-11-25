cb1860e8eba434554fccebb9b22276fb6be5a992
Karim Ouhammi <ouhammik@gmail.com>
2024-11-25 20:45:00 +0100

    update filter card

--- src/filter/views/FilterView.vue
+++ src/filter/views/FilterView.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="foo">
-        <v-expansion-panels elevation="0" class="pa-4" color="primary">
-            <v-expansion-panel eager>
-                <v-expansion-panel-title>Filter</v-expansion-panel-title>
-                <v-expansion-panel-text>
-                    <FilterCard
-                        :parking-garages-names="props.parkingGaragesNames"
-                        :filter="filter"
-                        @update-filter="(filter) => (parkingGarageStore.filter = { ...filter })"
-                    />
-                </v-expansion-panel-text>
-            </v-expansion-panel>
-        </v-expansion-panels>
+    <div class="d-flex justify-end">
+        <v-menu :close-on-content-click="false" location="bottom">
+            <template v-slot:activator="{ props }">
+                <v-btn v-bind="props"><v-icon>mdi-filter-outline</v-icon> </v-btn>
+            </template>
+
+            <FilterCard
+                :parking-garages-names="props.parkingGaragesNames"
+                :filter="parkingGarageStore.filter"
+                max-width="600"
+                @update-filter="(filter) => (parkingGarageStore.filter = { ...filter })"
+            />
+        </v-menu>
     </div>
 </template>
 
@@ -20,15 +20,12 @@ import { ParkingGarageName } from '@/parkingGarage/types/parkingGarageNames';
 import FilterCard from '../components/FilterCard.vue';
 import type { Filter } from '@/parkingGarage/types/filter';
 import { useParkingGarageStore } from '@/parkingGarage/parkingGarageStore';
-import { ref } from 'vue';
 
 const parkingGarageStore = useParkingGarageStore();
 
 const props = defineProps<{
     parkingGaragesNames: ParkingGarageName[];
 }>();
-
-const filter = ref<Filter>(parkingGarageStore.filter);
 </script>
 
 <style></style>
