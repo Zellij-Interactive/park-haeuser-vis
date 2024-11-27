751eb953bc128583a7c680a8f8aea6142f0e29cf
Karim Ouhammi <ouhammik@gmail.com>
2024-11-27 21:56:16 +0100

    update store and add index to filter

--- src/parkingGarage/parkingGarageStore.ts
+++ src/parkingGarage/parkingGarageStore.ts
@@ -2,15 +2,16 @@ import { defineStore } from 'pinia';
 import type { ParkingGarage } from './types/parkingGarage';
 import { parkingGarageService } from './parkingGarageService';
 import type { ParkingGarageName } from './types/parkingGarageNames';
-import { maxDate, minDate, type Filter } from './types/filter';
+import { filterMaxDate, filterMinDate, type Filter } from './types/filter';
 import { DateRange } from '@/core/dateRange';
 
 export const useParkingGarageStore = defineStore('parking-garage', {
     state: () => ({
         filter: {
             parkingGarages: [],
-            dateRange: new DateRange(minDate, maxDate),
+            dateRange: new DateRange(filterMinDate, filterMaxDate),
             showSHAPValues: false,
+            index: filterMinDate.getTime(),
         } as Filter,
         parkingGaragesMap: new Map<ParkingGarageName, ParkingGarage>(),
     }),
