105a4a9ec375ef3a414bc766ea16f9c2154619ee
Karim Ouhammi <ouhammik@gmail.com>
2024-11-30 16:52:18 +0100

    update main

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,28 @@ import App from './App.vue';
 
 // Vuetify
 import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
+import { createVuetify, type ThemeDefinition } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import { parkingGarageApi } from './parkingGarage/parkingGarageFakeApi';
 import colors from 'vuetify/util/colors';
 import 'vuetify/styles';
+
+const customLightTheme: ThemeDefinition = {
+    dark: false,
+    colors: {
+        primary: colors.cyan.darken4, // #006064
+        secondary: colors.blueGrey.lighten5, // #ECEFF1
+    },
+};
+
+const customDarkTheme: ThemeDefinition = {
+    dark: true,
+    colors: {
+        primary: colors.cyan.darken2, // #0097A7
+        secondary: colors.blueGrey.darken4, // #263238
+    },
+};
 
 const app = createApp(App);
 const vuetify = createVuetify({
@@ -21,20 +37,8 @@ const vuetify = createVuetify({
     theme: {
         defaultTheme: 'light',
         themes: {
-            light: {
-                dark: false,
-                colors: {
-                    primary: colors.cyan.darken4, // #006064
-                    secondary: colors.blueGrey.lighten5, // #ECEFF1
-                },
-            },
-            dark: {
-                dark: true,
-                colors: {
-                    primary: colors.cyan.darken2, // #0097A7
-                    secondary: colors.blueGrey.darken4, // #263238
-                },
-            },
+            light: customLightTheme,
+            dark: customDarkTheme,
         },
     },
 });
