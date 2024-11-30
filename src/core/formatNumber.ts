0538a3313099820f759b36c17a9089bdc17bbf52
Karim Ouhammi <ouhammik@gmail.com>
2024-11-30 17:41:43 +0100

    update formatNumber method

--- src/core/formatNumber.ts
+++ src/core/formatNumber.ts
@@ -1,10 +1,10 @@
-export function formatNumber(n: number | undefined) {
+export function formatNumber(n: number | undefined, maxFractionDigits: number) {
     if (n == null) {
         return;
     }
 
     return new Intl.NumberFormat('de-DE', {
         minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
+        maximumFractionDigits: maxFractionDigits,
     }).format(n);
 }
