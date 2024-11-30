d3f1f7b2acfa8153b7efdb9ccd6b907473d093c0
Karim Ouhammi <ouhammik@gmail.com>
2024-11-30 14:19:14 +0100

    format code

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 import DashboardView from './views/DashboardView.vue';
 import { useTheme } from 'vuetify';
 
