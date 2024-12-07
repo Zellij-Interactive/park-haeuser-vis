97d5c0fc4976adf7b460dd4368e5b102b81644de
Karim Ouhammi <ouhammik@gmail.com>
2024-12-07 22:24:31 +0100

    export some variables

--- src/legend/utils/ordinalScale.ts
+++ src/legend/utils/ordinalScale.ts
@@ -1,60 +1,10 @@
 import { _throw } from '@/core/_throw';
-import { ColorBlindMode, getColorPalette } from './colorBlindMode';
+import { ColorBlindMode, getColorPalette, paletteLength } from './colorBlindMode';
 import { scaleOrdinal } from 'd3';
 
-const defaultPalette = [
-    '#f7fbffff',
-    '#deebf7ff',
-    '#c6dbefff',
-    '#9ecae1ff',
-    '#6baed6ff',
-    '#4292c6ff',
-    '#2171b5ff',
-    '#08519cff',
-    '#083582ff',
-    '#022259ff',
-];
-
-const protanopiaPalette = [
-    '#fffcd4ff',
-    '#cde0caff',
-    '#b4d2c6ff',
-    '#82b6bcff',
-    '#69a8b7ff',
-    '#5a93a8ff',
-    '#3d6a89ff',
-    '#2e557aff',
-    '#102b5bff',
-    '#0d2644ff',
-];
-
-const deuteranopiaPalette = [
-    '#eae2bbff',
-    '#e9d7b8ff',
-    '#e8c9b3ff',
-    '#e8baaeff',
-    '#d7aba9ff',
-    '#c59ca4ff',
-    '#a48d9eff',
-    '#827d99ff',
-    '#606d94ff',
-    '#1d4e89ff',
-];
-
-const tritanopiaPalette = [
-    '#fcedfbff',
-    '#f1c1c5ff',
-    '#ecacaaff',
-    '#e18073ff',
-    '#db6a58ff',
-    '#c75c49ff',
-    '#a1412cff',
-    '#8d331dff',
-    '#661800ff',
-    '#510b00ff',
-];
-
-const paletteLength = defaultPalette.length;
+export const strokeOpacity = 0.4;
+export const strokeWeight = 1;
+export const strokeGap = 4;
 
 export const colorScale: any = (mode: ColorBlindMode) =>
     scaleOrdinal()
